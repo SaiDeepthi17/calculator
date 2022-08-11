ed6659b10a4210958cc50365edef193209259346
SaiDeepthi17 <106374872+SaiDeepthi17@users.noreply.github.com>
2022-08-11 14:19:23 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    float num1;
+    float num2;
+    char op;
+    float result;
+    printf("Enter the first number:");
+    scanf("%f",&num1);
+    printf("Enter the operation:");
+    scanf(" %c",&op);
+    printf("Enter the second number:");
+    scanf("%f",&num2);
+    switch(op)
+    {
+        case '-':
+        result = num1-num2;
+        printf("%f",result);
+        break;
+        case '+':
+        result = num1+num2;
+        printf("%f",result);
+        break;
+        case '*':
+        result = num1*num2;
+        printf("%f",result);
+        break;
+        case '/':
+        result = num1/num2;
+        printf("%f",result);
+        break;
+        default:
+        printf("The operator is not valid");
+    }
+    getch();
+}
